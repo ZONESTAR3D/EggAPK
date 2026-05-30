bd35ae013ba0d3055c7d5596b2a7cf53ac48c97f
Hally Zhong <hally@zonestar3d.com>
2026-05-30 17:37:11 +0800

    update in 2026-05-30

--- middleware.js
+++ middleware.js
@@ -7,16 +7,14 @@ export function middleware(context) {
   if (url.pathname.startsWith('/download/')) {
     // 提取请求路径，例如 /download/apk/file.apk  → /apk/file.apk
     const cosPath = url.pathname.replace('/download', '');
-    // 重写到 COS 的真实地址
-    const cosUrl = `https://eegg-1331241757.cos-website.ap-hongkong.myqcloud.com${cosPath}`;
+    // 使用自定义域名（绑定后生效）
+    const cosUrl = `https://download.zonestar3d.com${cosPath}`;
     return rewrite(cosUrl);
   }
 
-  // 其他请求正常处理
   return context.next();
 }
 
-// 可选：限制中间件只在 /download 路径下生效
 export const config = {
   matcher: '/download/:path*',
 };
